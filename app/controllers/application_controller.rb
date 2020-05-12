bb713dbba305c408a0dab13c21b68b3f3834f45e
lbirts <birtslauren@gmail.com>
2020-05-12 17:55:23 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,51 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/' do
+    "<h1>Welcome to your recipes</h1>"
+  end
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes/new' do
+    redirect "/recipes/new"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  post '/recipes' do
+    rec = Recipe.create(params)
+    redirect "/recipes/#{rec.id}"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  post '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    redirect "/recipes/#{@recipe.id}/edit"
+  end
+
+  patch '/recipes/:id' do
+    rec = Recipe.find(params[:id])
+    rec.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{rec.id}"
+  end
+
+  delete '/recipes/:id' do
+    Recipe.delete(params[:id])
+    redirect "/recipes"
+  end
 end
